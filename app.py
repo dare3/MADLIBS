853c52c4c0003d0765be5ee5769a146152f391e0
Dare3 <enitan_segun@yahoo.co.uk>
2024-06-27 18:01:32 -0400

    modification made to all files

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask,render_template, request
+from flask_debugtoolbar import DebugToolbarExtension
+import stories
+
+app=Flask(__name__)
+
+app.config['SECRET_KEY'] = "secret"
+debug = DebugToolbarExtension(app)
+
+#routes ==> homepage to display stories
+@app.route("/")
+def homepage():
+    return render_template("homepage.html", prompts=stories.story.prompts)
+
+@app.route("/story", methods=["POST"])
+def story():
+    text = stories.story.generate(request.form)
+    return render_template("story.html", text=text)
